a6cf6ef624ba3f9628cd7afefc8041a599acdb10
Jeremy Chanut <jeremy.chanut@masagroup.net>
2015-07-31 11:13:56 +0200

    Handling pending actions in the states stack.

--- includes/StateStack.hpp
+++ includes/StateStack.hpp
@@ -8,27 +8,52 @@
 #include <unordered_map>
 
 
+namespace sf
+{
+    class Event;
+    class Time;
+}
+
 class StateStack
 {
 public:
+    enum class Action
+    {
+        Push,
+        Pop,
+        Clear
+    };
+
     StateStack(State::Context context);
     ~StateStack() = default;
 
     template <typename T>
     void registerState(State::ID id);
 
+    void pushState(State::ID id);
+    void popState();
+    void clearStates();
+
+    void handleEvent(const sf::Event& event);
+    void update(const sf::Time& dt);
+    void render();
+
 private:
     State::Context context_;
-    std::unordered_map<State::ID, std::function<std::unique_ptr<State>()>> stateFactories_;
+
+    std::vector<std::unique_ptr<State>> statesStack_;
+
+    std::unordered_map<State::ID, std::function<std::unique_ptr<State>()>>  stateFactories_;
+    std::vector<std::pair<Action, State::ID>>                               pendingActions_;
 };
 
 
 template <typename T>
 void StateStack::registerState(State::ID id)
 {
-    stateFactories_[id] = [&context]()
+    stateFactories_[id] = [this]()
     {
-        return std::make_unique<T>(context_);
+        return std::make_unique<T>(*this, context_);
     };
 }
 
--- sources/StateStack.cpp
+++ sources/StateStack.cpp
@@ -6,3 +6,49 @@ StateStack::StateStack(State::Context context)
 {
 
 }
+
+void StateStack::pushState(State::ID id)
+{
+    pendingActions_.push_back(std::make_pair(Action::Push, id));
+}
+
+void StateStack::popState()
+{
+    pendingActions_.push_back(std::make_pair(Action::Pop, State::ID::None));
+}
+
+void StateStack::clearStates()
+{
+    pendingActions_.push_back(std::make_pair(Action::Clear, State::ID::None));
+}
+
+void StateStack::handleEvent(const sf::Event& event)
+{
+    for (const auto& pair : pendingActions_)
+    {
+        switch (pair.first)
+        {
+        case Action::Push:
+            statesStack_.push_back(stateFactories_[pair.second]());
+            break;
+        case Action::Pop:
+            statesStack_.pop_back();
+            break;
+        case Action::Clear:
+            statesStack_.clear();
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+void StateStack::update(const sf::Time& dt)
+{
+
+}
+
+void StateStack::render()
+{
+
+}
